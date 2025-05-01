f3c52e19dcca4af1559b384cc39951b33d2c3d8a
Edmilzon <edmilzon.luna@gmail.com>
2025-05-01 10:58:14 -0400

    configuration inicial clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import {
   UserButton,
 } from '@clerk/nextjs'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -34,7 +33,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
+          <header >
             <SignedOut>
               <SignInButton />
               <SignUpButton />
